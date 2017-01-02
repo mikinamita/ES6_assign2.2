90aed2946d89a3ae091954ca5ee072b051cf2cc9
mikinamita <mikinamita@gmail.com>
2017-01-02 23:30:06 +0530

    Add files via upload

---
+++ assign2.2main.js
@@ -0,0 +1,3 @@
+
+import {add,sub,mul,div} from 'calculation';
+console.log(add(4,8));
---
+++ calculation.js
@@ -0,0 +1,19 @@
+export function add(x,y)
+{
+return x+y;
+}
+
+export function sub(a,b)
+{
+return a-b;
+}
+
+export function mul(p,q)
+{
+return p*q;
+}
+
+export function div(m,n)
+{
+return m/n;
+}
